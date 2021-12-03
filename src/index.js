d6bf95a06b7a040d209f3f3f76a32f65c9a21993
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-12-02 19:11:57 -0500

    first two deliverables met

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const ramenMenu = document.querySelector('div#ramen-menu')           // store node where we want to add all ramen images in
+    const selectedImage = document.querySelector('img.detail-image')
+    const selectedName = document.querySelector('h2.name')
+    const selectedRestaurant = document.querySelector('h3.restaurant')
+    const selectedRating = document.querySelector('span#rating-display')
+    const selectedComment = document.querySelector('p#comment-display')
+
+    fetch('http://localhost:3000/ramens')
+    .then((response) => response.json())
+    .then((ramenData) => {
+        ramenData.forEach((ramenItem) => {
+            const ramenImage = document.createElement("img")
+            ramenImage.setAttribute('src', `${ramenItem.image}`)
+            ramenMenu.appendChild(ramenImage)
+            ramenImage.addEventListener('click', (e) =>{
+                selectedImage.setAttribute("src", e.target.src)
+                selectedName.innerText= ramenItem.name
+                selectedRestaurant.innerText= ramenItem.restaurant
+                selectedRating.innerText = ramenItem.rating
+                selectedComment.innerText = ramenItem.comment
+
+            })
+        })
+        
+    
+    
+    
+    })
+
+
+})
